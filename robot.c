ec69256a656981798ec517a846c278c1ff47f29d
murrayse23 <murrayse23@engineering06.campus.up.edu>
2022-04-21 16:48:11 -0700

    Wrote dunce on Dalek's forehead. Added my name to the authors.

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw, Nathan Schmedake
+ * @author Tanya L. Crenshaw, Nathan Schmedake, Sean Murray
  * @since August 2013
  *
  */
@@ -56,7 +56,7 @@ void dalekPrintAscii(void)
 	 "I am Dalek, from Dr. Who\n"
 	 "%s      <----->      \n" 
          "%s     <  (0)  >     \n"        
-         "%s     |       |     \n"
+         "%s     | dunce |     \n"
          "%s    < ------- >    \n"
          "%s    o         o    \n"
          "%s    o  0  ()  o    \n"
